5743729f3fe6cd08315efd10077f726d2e44fbcb
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-11 20:08:17 +0200

    Add files via upload

---
+++ eerstejs.js
@@ -0,0 +1,36 @@
+
+
+console.log ('Hello Winc Academy');
+   let myName='Lars';
+    console.log ('name');
+let firstNo='4';
+let secondNo="4";
+console.log (firstNo + secondNo);
+
+let x=4;
+let y=4;
+let z=x+y;
+console.log (z=x+y);
+
+let a=1;
+let b=3;
+let c=b-a;
+console.log (c=b-a);
+
+let d=5;
+let e=3;
+let f=d * e;
+console.log (f=d*e);
+
+let g=5;
+let h=3;
+let i=g/h;
+console.log (i=g/h);
+
+let j=100;
+let k=15;
+let l=k%j;
+console.log (l=k%j);
+
+let age=48;
+console.log ('age');
