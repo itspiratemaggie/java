58f99764800edea956717a8bea370c982707f93a
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-12 14:44:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log ("Hallo Winc Academy Studenten");
+/* let a=1230941;
+let b=1823794;
+let c=a * b;
+console.log (c=a*b);
+// hieronder vind je een grote som
+let d=637263;
+let e=54;
+let f=d/e;
+console.log (f=d/e); */
+
