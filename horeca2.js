fd562edd161e43c4338dc83cc5763a5d5e1e3d94
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-13 16:36:47 +0200

    Add files via upload

---
+++ horeca2.js
@@ -0,0 +1,9 @@
+let visitorAge=16;
+if (visitorAge>18) {document.write ('You are over 18 and good to go!'); console.log ('Welcome in!');}
+else if (visitorAge<18) {document.write ('You are under 18 and can not come inside'); console.log ("Nice try son but stay out");}
+
+const isFemale=true;
+if (isFemale=true) {document.write ("Welcome to Ladies Night"); console.log ("Welcome to Ladies Night");} else if (isFemale=false) {document.write ("Nice try bloke, but this is not for you"); console.log ("Nice try bloke, but this is not for yoy");}
+
+const driverStatus=bob;
+if (driverStatus=bob) {document.write ("Customer may drive a vehicle"); console.log ('Customer may drive a vehicle only when bob');}
