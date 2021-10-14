8fbab9c76c80ab16442fab9d1496aad04cfffe94
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-14 14:28:49 +0200

    Add files via upload

---
+++ kortingsactie.js
@@ -0,0 +1,11 @@
+let customerAge=16;
+if (customerAge >=18 && customerAge<=25) {console.log ('Jij krijgt 50 procent korting!');}
+else {console.log ('Jij betaalt vandaag de volle mep');}
+
+const firstName="Bram";
+if (firstName="Bram" || firstName="Sarah") {console.log ("Jij krijgt vandaag een gratis biertje!");} else {console.log ("Jij krijgt vandaag geen gratis biertje")}
+
+const totalAmount=50;
+if (totalAmount >=25) {console.log ('Jij krijgt een gratis portie bitterballen');} if (totalAmount>=50) {console.log ('Jij krijgt een gratis portie nachos');}
+if (totalAmount >=100) {console.log ('Jij krijgt een gratis fles champagne!');}
+
