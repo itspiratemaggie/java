c05045dcef9557e2cda158d283470ad96cc7efe2
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-15 13:27:32 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,19 @@
+const makePizza=function(){
+    console.log ("Maak je deeg");
+    console.log ("Beleg het deeg met tomaat en zo");
+    console.log ('Doe de pizza in de oven');
+};
+
+makePizza ();
+makePizza ();
+
+
+const makeSushi=function (){
+    console.log ("Slice up fresh fish");
+    console.log ("Boil Rice");
+    console.log ("Put slice of fish and rice together");
+};
+
+makeSushi ();
+makeSushi ();
+makeSushi ();
