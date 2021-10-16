be05c558c32e900ace3543f2c510064817655c36
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-16 08:40:04 +0200

    Add files via upload

---
+++ functions2.js
@@ -0,0 +1,17 @@
+const paintWall=function(paint){
+    console.log ("Buy paint in color:" + paint);
+    console.log ("Grind the" + houseSide);
+    console.log ('Put on grounding layer');
+    console.log ("Put on color:" + paint)
+    console.log ("Your " + houseSide + "has been painted in:" + paint)
+    
+};
+
+const houseSide = "north wall"
+
+
+paintWall ('green');
+paintWall ('yellow');
+
+
+
