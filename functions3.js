33ada682fe74695ab4abda6622074209f6cbed39
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-18 19:48:38 +0200

    Add files via upload

---
+++ functions3.js
@@ -0,0 +1,25 @@
+function squareOne (number1,number2, sum1, sum2) {const sum1=number1 * number2; 
+    const sum2=sum1 * sum1;
+const number1=2;
+const number2=3;
+return sum2;}
+
+squareOne ();
+
+const squareTwo=function (number3, number4, sum3, sum4) {const sum1=number1 * number2; 
+    const sum4=sum3 * sum3;
+const number3=2;
+const number4=3;
+return sum4;};
+
+squareTwo ();
+
+const squareThree =(number5, number6, sum 5, sum6) => {const sum5=number5 * number6; 
+    const sum6=sum5 * sum5;
+const number5=2;
+const number6=3;
+return sum4;};
+
+
+squareThree ();
+ 
