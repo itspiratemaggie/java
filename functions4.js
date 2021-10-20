aebe87345259ff08aba080e5baa95d719609aee2
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-20 16:07:25 +0200

    Add files via upload

---
+++ functions4.js
@@ -0,0 +1,40 @@
+const customerAge=function (custAge){ if (custAge => 18) {
+    return "true";
+} else {
+    return "false";
+}
+};
+
+customerAge (16);
+
+const customerGreet=function (number)  {if (number => 18) {customerAge ();
+    return "Hello there!";
+} else {
+    return "Hey Kiddo";
+} 
+};
+
+const calculateVat=function (basePrice, number) {const basePrice= 100;
+let number = 0.21; 
+calculateAmount (); };
+
+const calculateAmount=function (basePrice, number) {return basePrice * number;}
+
+calculateVat (100);
+
+const calculateVat2=function (percent){
+    
+    const number = 0.21; 
+    const totalPrice=basePrice * number;
+    return totalPrice;};
+
+    
+    const onlyVat=function (percent) {calculateVat2();
+    const percent=0.21
+    return percent/totalPrice;};
+    
+    calculateVat (100);
+
+
+
+
