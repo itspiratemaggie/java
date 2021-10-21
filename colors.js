b06eacb4bc61f1b0bde62f07db4029a10d91cb08
WincLars <88272651+itspiratemaggie@users.noreply.github.com>
2021-10-21 16:13:17 +0200

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,34 @@
+let colors=['yellow', "orange","red","blue"]
+
+for (index = 0; index < array.length; index++) {
+    console.log(array[index]);
+}
+
+while (index < array.length) {
+    console.log(array[index]);
+    index++;
+
+
+let colors=['yellow', "orange","red","blue"]
+
+colors.prototype.forEach()
+
+//for loop 2 regels
+//while loop 3 regels
+//foreach 1 regel
+//foreach is minder foutgevoelig
+
+var obj = {
+    name: "Simon",
+    age: "20",
+    clothing: {
+        style: "simple",
+        hipster: false
+    }
+}
+
+for(var propt in obj){
+    console.log(propt + ': ' + obj[propt]);
+}
+
+//nee
